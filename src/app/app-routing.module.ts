1afc179a6efefc76e733ed94a7f3d13c2e0dba14
sumit <34721357+sumitsarraf@users.noreply.github.com>
2022-04-29 16:49:14 +0530

    angular firebase login
    .

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,22 @@
+import { RegisterComponent } from './component/register/register.component';
+import { DashboardComponent } from './component/dashboard/dashboard.component';
+import { LoginComponent } from './component/login/login.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { VarifyEmailComponent } from './component/varify-email/varify-email.component';
+import { ForgotPasswordComponent } from './component/forgot-password/forgot-password.component';
+
+const routes: Routes = [
+  {path:'',redirectTo:'login',pathMatch: 'full'},
+  {path:'login', component:LoginComponent},
+  {path:'dashboard', component:DashboardComponent},
+  {path:'register', component:RegisterComponent},
+  {path: 'varify-email', component : VarifyEmailComponent},
+  {path: 'forgot-password', component : ForgotPasswordComponent},
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/component/dashboard/dashboard.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+
+import { AuthService } from 'src/app/shared/auth.service';
+
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css']
+})
+export class DashboardComponent implements OnInit {
+
+  constructor(private auth: AuthService) { }
+
+  ngOnInit(): void {
+   
+  }
+
+   logoutBtn() {
+     this.auth.logout();
+   }
+
+
+
+
+
+}
